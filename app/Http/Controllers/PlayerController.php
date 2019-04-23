21f8233634358a6fdcee1e4c804e73120fd50a12
dhairya <dhairyasenjaliya@gmail.com>
2019-04-23 14:25:50 +0530

    FIx

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -46,7 +46,6 @@ class PlayerController extends Controller
      */
     public function store(Request $request)
     {    
-        dd($request);
         $request->validate([
             'name'=>'required' ,
             'national_team'=>'required',
