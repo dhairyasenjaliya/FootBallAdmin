523adc52bb8028706a0a750dac416d835a7ad0ec
dhairya <dhairyasenjaliya@gmail.com>
2019-04-24 13:17:21 +0530

    Api get

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 
-Route::post('playerinfo', 'ApiPlayerController@playerinfo'); 
+Route::get('playerinfo', 'ApiPlayerController@playerinfo'); 
